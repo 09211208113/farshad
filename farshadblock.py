a96973c05b5ee81e259af9b2cd1431e7d3475ed1
09211208113 <71493467+09211208113@users.noreply.github.com>
2022-06-21 15:21:20 +0430

    Update farshadblock.py

--- farshadblock.py
+++ farshadblock.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from time import sleep
-
 feri = 'BAC2WUezDaB-CR63VY8xPKqRIFOVCR1LPTuLm7hxi2UMajFIvb3IlTNnJhZPtubtK8s5urXxlQ00qavoayF1yAXSIJ_3SL8SCZkLXA5oE9I8M5k2V3PcivjinEWAZgdcFgCmfdprNfKBPSfldoLhYIlLB6Ilq5urxuD-tEEwr-QE9dAT3UJA6ka70-JxN5eWqIYJQd-ZtxaXH9qpkYeJptygMUjGgE474GctsupS5SMYr0hVa3TDuk7W2Ex2sJfATsKKFxt4CWlgopy1q1e1JihNk-7kVUTq0peY4jyxkmjUPUdfNkxfk1yqKjgkuXFHBse_H0R5KU8XbAXtvg6fFVrCAAAAAH30Hh0A'
 
 app = Client(session_name=feri, api_id=11434929, api_hash='96015db8ea30bdbbeeded8a6c046d3fa')
