fe0ecc516c56f34252ab55404128eb4699693190
09211208113 <71493467+09211208113@users.noreply.github.com>
2022-06-21 16:50:14 +0430

    Update jointabar.py

--- jointabar.py
+++ jointabar.py
@@ -3,27 +3,20 @@ from pyrogram.types import Message
 from time import sleep
 
 
-feri = 'BABUxuoXA_F_x7IxPQ1h3THXCQYxLmNPcsZ5DjTF0lQQOX6Qk_9Ik6LHFCfTdpmERrU0GZw1_opt7Umk_Sr_Jd0067nQDu-9bZ8ALeOeRaKq7_-OJ8TGcsynnc0ee6seiIls9PDvOPWPfqsX72J8KZbu9C082BjtMQ7ma5c9chxaM9G7Gr1XzbE-nlPwH6qu93oKymGHumhcW_zngTo_PBFrxGi6CovJeUfb3Uz-wNrsK3ZkrfA8Vh2VyoalThBKwW2P8Op5613VRv9G4ALMc7Moy5PAG0cLgLbwSvRzjJD501jo2vhdv7VPKz7lzFDcpbBMZ9iHFjF7TDbbOgHTYxmrAAAAATbMW4QA'
+feri = 'AQB-9RF0ZJ8C4BmAJFp3iLtDsN6Of_cplgxexaw2q_Y16m86AlITiw6m6b17ycH3u4Efthva4HC2n8qv-Ky9dCo5goj21uGpMrKidSIXojttYqTjaPIVAasG9EYAXArfojKqBAyOFtHiJlwklTNl0EP5YGZB4ndv1baKtT7_4kJJhmke_QMkpQpOS7Mv9ouNCyuGCPim8lYWMrRAyuW84yFqJdu-00jZl0Og_aOvzKvrnesiR79r3jenzDTw8KzNUIIo6R0Kb7LSS6Dq8rfS-bEjI1LBIKLLdh3CKgoDopa9w2v8o8XVRWWF1SceuvXRzeHPxUJ1oVTqsu9lH2jm2BBAAAAAATs5ToYA'
 
-app = Client(session_name=feri, api_id=3458298, api_hash='fb15460b27d133024fbcba9a8e1d0cb3')
+app = Client(session_name=feri, api_id=13970534, api_hash='b610e73718c72d2e6148124696d72361')
    
    
 wwbot = [491459293, 175844556]
-gp = [-1001750093119]
+gp = []
 
-@app.on_message(filters.command(['speedjoin'],None))
-def spd(client, message):
-    global speed
-    try:
-        speed = int(message.command[1])
-        message.reply_text('**Speed  to {} Changed ✅**'.format(speed))
-    except Exception:
-        message.reply_text('**برای تنظیم سرعت به این شکل عمل کنید\nspeed NUMBER**')
-         
+
 @app.on_message(filters.command('join', '!') & filters.me)
 def join_game(_, m: Message):
     if len(m.command) == 3:
-        chat_id = m.command[1]
+        chat_id = int(m.command[1])
+        print(chat_id)
         if m.command[2].lower() == 'on':
             if chat_id not in gp:
                 gp.append(chat_id)
@@ -42,13 +35,13 @@ def join_game(_, m: Message):
         m.edit_text('**دستور صحیح ارسال گردد**')
 
 
-@app.on_message(filters.caption & filters.inline_keyboard)
-def doore(app: Client, m: Message):
-    if m.from_user.id in wwbot and m.chat.id in gp:
+@app.on_message(filters.caption & filters.inline_keyboard & filters.user(wwbot))
+def sjoin(app: Client, m: Message):
+    if int(m.chat.id) in gp:
         link = m.reply_markup.inline_keyboard[0][0].url
         link = link.split("=")[1]
-        sleep(speed)
-        app.send_message(175844556, f"/start {link}")
+        sleep(3)
+        app.send_message(m.from_user.id, f"/start {link}")
 
 
 app.run()
